8b48d5be262e4d9ba297d3fd466dd3fb8c574322
costaa17 <toiavalle@gmail.com>
2017-01-31 20:00:49 -0800

    Initial Commit

---
+++ My Great Productivity App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Great Productivity App
+//
+//  Created by Ana Vitoria do Valle Costa on 1/31/17.
+//  Copyright © 2017 Ana Vitoria do Valle Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
